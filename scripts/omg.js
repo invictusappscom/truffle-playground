da1a0ea7bdf2258676c1c32008e5de27fb12f103
Zarko Jevtic <zarej@svrljig.net>
2021-08-06 16:37:52 +0200

    Added OmiseGo

--- scripts/omg.js
+++ scripts/omg.js
@@ -1,12 +1,12 @@
-const contract = artifacts.require('Dai')
+const contract = artifacts.require('Omg')
 
-const contractDaiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+const contractOmgAddress = '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07'
 
 const toMyAddress = '0x915A9A5A83C8667842E1B5164a8f2Ca576c2617C'
-const fromRichAddress = '0x47ac0fb4f2d84898e4d9e7b4dab3c24507a6d503'
+const fromRichAddress = '0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8' // Binance addr has a lot of tokens
 
 module.exports = async (callback) => {
-  const ins = await contract.at(contractDaiAddress)
+  const ins = await contract.at(contractOmgAddress)
 
   // console.log(ins)
   const amount = web3.utils.toWei('20')
--- scripts/weth.js
+++ scripts/weth.js
@@ -1,16 +1,16 @@
-var contract = artifacts.require('Weth')
+const contract = artifacts.require('Weth')
 
-var contractWethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+const contractWethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
 
 const toMyAddress = '0x915A9A5A83C8667842E1B5164a8f2Ca576c2617C'
 const fromRichAddress = '0xe5F8086DAc91E039b1400febF0aB33ba3487F29A'
 
 module.exports = async (callback) => {
-  let ins = await contract.at(contractWethAddress)
+  const ins = await contract.at(contractWethAddress)
 
   try {
     const amount = web3.utils.toWei('20')
-    let result = await ins.contract.methods.transfer(toMyAddress, amount).send({ from: fromRichAddress })
+    const result = await ins.contract.methods.transfer(toMyAddress, amount).send({ from: fromRichAddress })
 
     console.log(result)
   } catch (e) {
